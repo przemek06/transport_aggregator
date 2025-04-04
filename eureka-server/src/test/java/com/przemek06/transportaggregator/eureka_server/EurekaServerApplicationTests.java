221ea95f2dbea358300126bc128465d2f2d3f4fb
Mihika <mihikasrivastava22@gmail.com>
2025-04-05 03:07:20 +0530

    code: interim service registry implemented using eureka.

---
+++ eureka-server/src/test/java/com/przemek06/transportaggregator/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przemek06.transportaggregator.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- polregio/src/main/java/edu/pg/polregio/PolregioApplication.java
+++ polregio/src/main/java/edu/pg/polregio/PolregioApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PolregioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PolregioApplication.class, args);
 	}
