7bdee37cf94a929157f3bcde8e649accba8094a2
mihika04 <mihikasrivastava22@gmail.com>
2025-06-04 23:21:12 +0200

    front end changes made

--- client/src/TopDestinations.js
+++ client/src/TopDestinations.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
 
 export default function TopDestinations() {
   const [topDestinations, setTopDestinations] = useState([]);
